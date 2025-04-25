392358661fc4bde7626bfbb15f22f8d7bd31c494
achyu-dev <achyugeorge7744@gmail.com>
2025-04-25 21:10:10 +0530

    profile tab complete

--- services/userService.ts
+++ services/userService.ts
@@ -1,16 +1,33 @@
 import { firestore } from "@/config/firebase";
-import { UserDataType, ResponseType} from "@/types";
+import { UserDataType, ResponseType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFiletoCloudinary } from "./imageService";
 
-export const updateUser = async (uid: string, 
-    updatedData: UserDataType): Promise<ResponseType> => {
-        try{
-            // image is not updated here, only name is updated
-            const userRef = doc(firestore, "users", uid);
-            await updateDoc(userRef, updatedData);
-            return { success: true, msg: "User data updated successfully" };
-        } catch (error:any) {
-            console.error("Error updating user data:", error);
-            return { success: false, msg: "Failed to update user data" };
-        }
+export const updateUser = async (
+  uid: string,
+  updatedData: UserDataType
+): Promise<ResponseType> => {
+  try {
+    if (updatedData?.image || updatedData?.image?.uri) {
+      const imageUploadres = await uploadFiletoCloudinary(
+        updatedData.image,
+        "users"
+      );
+      if (!imageUploadres.success) {
+        return {
+          success: false,
+          msg: imageUploadres.msg || "Failed to upload image",
+        };
+      }
+
+      updatedData.image = imageUploadres.data;
     }
+
+    const userRef = doc(firestore, "users", uid);
+    await updateDoc(userRef, updatedData);
+    return { success: true, msg: "User data updated successfully" };
+  } catch (error: any) {
+    console.error("Error updating user data:", error);
+    return { success: false, msg: "Failed to update user data" };
+  }
+};
